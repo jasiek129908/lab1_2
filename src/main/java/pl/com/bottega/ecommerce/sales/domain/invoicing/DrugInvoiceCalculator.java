fd51f7fe45eecef78776cc3f3126e14defbabd13
Jan Oleksik <222529@edu.p.lodz.pl>
2021-03-15 17:55:37 +0100

    Poprawa klasy StandardInvoiceCalculator i dodanie klasy DrugInvoiceCalculator

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DrugInvoiceCalculator.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DrugInvoiceCalculator.java
@@ -5,20 +5,20 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import java.math.BigDecimal;
 
-public class StandardInvoiceCalculator implements InvoiceCalculator {
+public class DrugInvoiceCalculator {
 
     private Money net;
     private RequestItem item;
 
-    public StandardInvoiceCalculator(Money net, RequestItem item) {
+    public DrugInvoiceCalculator(Money net, RequestItem item) {
         this.net = net;
         this.item = item;
     }
 
     @Override
     public InvoiceLine calculate(ProductType productType, Money net) {
-        BigDecimal ratio =  BigDecimal.valueOf(0.23);
-        String desc = "23%";
+        BigDecimal ratio =  BigDecimal.valueOf(0.05);
+        String desc = "5% (D)";
         Money taxValue = net.multiplyBy(ratio);
         Tax tax = new Tax(taxValue, desc);
         return new InvoiceLine(item.getProductData(), item.getQuantity(), net, new Tax(taxValue, desc));
